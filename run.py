086ffe85f2ac64544faa721428416317e8de3e45
Wei Wang <ww85@uw.edu>
2015-08-13 16:07:13 -0700

    add sqlite database and test it

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import time
 from os import listdir
 from pymediainfo import MediaInfo
 from pprint import pprint
+import sqlite3
 
 disk_path="/Volumes/NO NAME"
 #disk_path="./testdir"
@@ -13,25 +14,38 @@ disk_path="/Volumes/NO NAME"
 
 #print files
 
-for dirname, dirnames, filenames in os.walk(disk_path):
-    for file in filenames:
-        filename = os.path.join(dirname,file)
-        file_stem, file_ext = os.path.splitext(filename)
-        if file_ext == ".MP4":
-            #print(file_stem, file_ext)
-            file_size = os.path.getsize(filename)
-            file_size_mb = file_size >> 20
-            file_mtime = os.path.getmtime(filename)
-            file_mtime_h = time.ctime(file_mtime)
-            if file_size_mb > 1000:
-                media_info = MediaInfo.parse(filename)
-                for track in media_info.tracks:
-                    if track.track_type == 'Video':
-                        print track.width, track.height, track.duration, file_mtime_h, file_mtime, filename
+def scan_media():
+    for dirname, dirnames, filenames in os.walk(disk_path):
+        for file in filenames:
+            filename = os.path.join(dirname,file)
+            file_stem, file_ext = os.path.splitext(filename)
+            if file_ext == ".MP4":
+                #print(file_stem, file_ext)
+                file_size = os.path.getsize(filename)
+                file_size_mb = file_size >> 20
+                file_mtime = os.path.getmtime(filename)
+                file_mtime_h = time.ctime(file_mtime)
+                if file_size_mb > 1000:
+                    media_info = MediaInfo.parse(filename)
+                    for track in media_info.tracks:
+                        if track.track_type == 'Video':
+                            print track.width, track.height, track.duration, file_mtime_h, file_mtime, filename
                         # track.other_duration
                         #pprint(track.to_data())
                         #break
                         
+                        
+def test_db():
+    conn = sqlite3.connect("media.db")
+    c = conn.cursor()
+    c.execute('SELECT * from sqlite_master WHERE type="table"')
+    print c.fetchone()
+    conn.close()
+
+#scan_media()
+test_db()
+print "done"
+
                         #print track.bit_rate, track.bit_rate_mode, track.codec, filename
                 #print(filename, file_size_mb , "MB")
             
