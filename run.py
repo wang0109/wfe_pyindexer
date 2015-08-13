9f889168c24beed3fb32e41c61e4fc396208c112
Wei Wang <ww85@uw.edu>
2015-08-13 15:04:58 -0700

    list mp4 files larger than 1000MB

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
+import os
 from os import listdir
 
 disk_path="/Volumes/NO NAME"
+#disk_path="./testdir"
 
-files = [ f for f in listdir(disk_path) ]
 
-print files
+#files = [ f for f in listdir(disk_path) ]
+
+#print files
+
+for dirname, dirnames, filenames in os.walk(disk_path):
+    for file in filenames:
+        filename = os.path.join(dirname,file)
+        file_stem, file_ext = os.path.splitext(filename)
+        if file_ext == ".MP4":
+            #print(file_stem, file_ext)
+            file_size = os.path.getsize(filename)
+            file_size_mb = file_size >> 20
+            if file_size_mb > 1000:
+                print(filename, file_size_mb , "MB")
+            
+        #print(subdir)
+
+# list all dirs that has at least one MP4 file, and filesize greater than 2GB
